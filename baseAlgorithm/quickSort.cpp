f9995ff57eda9207335ed57b7c62fc87e02d68d5
fengqian <401861566@qq.com>
2017-09-07 11:55:20 +0800

    modify quickSort

--- baseAlgorithm/quickSort.cpp
+++ baseAlgorithm/quickSort.cpp
@@ -2,14 +2,14 @@
 
 void quickSort(int *array, int startIndex, int endIndex);
 void printfIntArray(int *array, int length);
+void quickSort2(int *array, int startIndex, int endIndex);
 
 int main()
 {
     printf("hello\n");
-
-    int a[8] = {5, 8, -4, 15, 8, 1, 9, -20};
+    int a[8] = {5, 8, -4, 15, 8, 1, 9, 5};
     printfIntArray(a, 8);
-    quickSort(a,0,7);
+    quickSort2(a,0,7);
     printfIntArray(a, 8);
     getchar();
     return 0;
@@ -40,12 +40,38 @@ void quickSort(int *array, int startIndex, int endIndex)
 {
     if(startIndex < endIndex)
     {
+        //int r = partition(array, startIndex, endIndex);
         int r = partition(array, startIndex, endIndex);
         quickSort(array, startIndex, r - 1);
         quickSort(array, r + 1, endIndex);
     }
 }
 
+void quickSort2(int *array, int startIndex, int endIndex)
+{
+    if (startIndex >= endIndex)
+        return;
+    
+    int key = array[startIndex];
+    int l = startIndex, r = endIndex;
+    
+    while(l < r){
+        while(l < r && array[r] > key){
+            r--;
+        }
+        array[l] = array[r];
+
+        while(l < r && array[l] <= key){
+            l++;
+        }
+        array[r] = array[l];
+    }
+    array[l] = key;
+
+    quickSort2(array, startIndex, l - 1);
+    quickSort2(array, l + 1, endIndex);
+
+}
 void printfIntArray(int *array, int length)
 {
     for (int i = 0; i < length; i++)
